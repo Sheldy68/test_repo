93abc86c3e7e7ebfed59221574ba62c2deb4b641
KorotaevaSA <sheldy68@gmail.com>
2024-05-06 19:00:42 +0300

    added .gitignore file and src/ folder

---
+++ src/MainGit.java
@@ -0,0 +1,8 @@
+public class MainGit {
+    public static void main(String[] args) {
+        /*многострочный
+        комментарий
+         */
+        System.out.println("Hello World!");
+    }
+}
