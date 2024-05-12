ba5b56f295d80502ad285755d9ca01d40f8dfc6f
KorotaevaSA <sheldy68@gmail.com>
2024-05-12 14:03:51 +0300

    add bye-bye

--- src/MainGit.java
+++ src/MainGit.java
@@ -5,6 +5,9 @@ public class MainGit {
          */
         System.out.println("Hello World!");
         System.out.println("Hello Sveta!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello!");
+        }
         for (int i = 0; i < 10 ; i++) {
             System.out.println("Privet!");
         }
