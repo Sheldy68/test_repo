10df346149649738a273ac5d8d4cab7769c9d8c8
KorotaevaSA <sheldy68@gmail.com>
2024-05-12 13:59:47 +0300

    sout privet 10 times

--- src/MainGit.java
+++ src/MainGit.java
@@ -5,5 +5,8 @@ public class MainGit {
          */
         System.out.println("Hello World!");
         System.out.println("Hello Sveta!");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("Privet!");
+        }
     }
 }
